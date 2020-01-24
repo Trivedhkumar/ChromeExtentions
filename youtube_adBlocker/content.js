54653425918614dcc312613df9f87887a67aa96f
Trivedh Kumar <35686666+Trivedhkumar@users.noreply.github.com>
2020-01-24 19:31:55 +0530

    Add files via upload

---
+++ youtube_adBlocker/content.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  var skip_button = document.getElementsByClassName(
+    "ytp-ad-skip-button ytp-button"
+  )[0];
+  if (skip_button != null) skip_button.click();
+}, 1000);
